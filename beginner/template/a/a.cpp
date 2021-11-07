251649ad49576f8f65517d8105722cb76bf26010
Masahito Kumada <kumada.masahito@gmail.com>
2021-11-07 15:24:02 +0900

    add template folder.

---
+++ beginner/template/a/a.cpp
@@ -0,0 +1,20 @@
+// #include <bits/stdc++.h>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include <numeric>
+#include <math.h>
+using namespace std;
+ 
+using ll=long long;
+using p_ii=pair<ll,ll>;
+ 
+#define REP(i,n) for(int i=0;i<(n);i++)
+
+
+int main() {
+
+
+	return 0;
+}
---
+++ beginner/template/b/b.cpp
@@ -0,0 +1,20 @@
+// #include <bits/stdc++.h>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+#include <numeric>
+using namespace std;
+ 
+using ll=long long;
+using p_ii=pair<ll,ll>;
+ 
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define REP_IF(i,j,n) for(int i=j;i<(n);i++)
+
+
+int main(){
+
+
+    return 0;
+}
